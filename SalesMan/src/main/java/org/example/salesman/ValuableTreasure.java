779464aafa425ca2b933d7ec3a9551ea5e70e44a
Blackbeard3978 <116648728+Blackbeard3978@users.noreply.github.com>
2024-03-10 16:38:00 -0400

    Merge remote-tracking branch 'origin/master'

--- SalesMan/src/main/java/org/example/salesman/ValuableTreasure.java
+++ SalesMan/src/main/java/org/example/salesman/ValuableTreasure.java
@@ -15,17 +15,61 @@ public class ValuableTreasure {
     private static int CrystalGoblets=0;
     private static int WoodenBow=0;
     private static int GoldenGoblet=0;
-    private static int PaladinShield=0;
+    private static int PaladinsShield=0;
     private static int GoldenKey=0;
     private static int DragonsScroll=0;
 
 
     public ValuableTreasure(int treasureId) {
+
         this.treasureId = treasureId;
+        if(treasureId==1&&DiamondRing==0){
+            treasureName="Diamond Ring";
+            valueOfTreasure=1000;
+            DiamondRing++;
+        } else if (treasureId==2&&JewelEncrustedSword==0) {
+            treasureName="Jewel Encrusted Sword";
+            valueOfTreasure=2000;
+            JewelEncrustedSword++;
+        }
+        else if(treasureId==3&&CrystalGoblets==0){
+            treasureName="Crystal Goblets";
+            valueOfTreasure=5000;
+            CrystalGoblets++;
+        }
+        else if(treasureId==4&&WoodenBow==0){
+            treasureName="Wooden Bow";
+            valueOfTreasure=100;
+            WoodenBow++;
+        }
+        else if(treasureId==5&&GoldenGoblet==0){
+            treasureName="Golden Goblet";
+            valueOfTreasure=500;
+            GoldenGoblet++;
+        }
+        else if(treasureId==6&&PaladinsShield==0){
+            treasureName="Paladin's Shield";
+            valueOfTreasure=700;
+            PaladinsShield++;
+        }
+        else if(treasureId==7&&GoldenKey==0){
+            treasureName="Golden Key";
+            valueOfTreasure=300;
+            GoldenKey++;
+        }
+        else if(treasureId==8&&DragonsScroll==0){
+            treasureName="Dragon's Scroll";
+            valueOfTreasure=7500;
+            DragonsScroll++;
+
+        }
+        else{
+            treasureName="Empty";
+            valueOfTreasure=0;;
+
     }
 
-    public int getTreasureId() {
-        return treasureId;
+
     }
 
     public void setTreasureId(int treasureId) {
@@ -48,5 +92,12 @@ public class ValuableTreasure {
         this.yCoordinate = yCoordinate;
     }
     public int getValueOfTreasure(){return valueOfTreasure;}
-}
+    public void setValueOfTreasure(int value){valueOfTreasure=value;}
+
+    //Constructor that prevents the occurance of more than 1 of each unique treasure
+    //Each treasure has a unique number assigned to it from 1-8. Any other value
+    //will result in the treasure being assigned a value of zero and the name "Empty"
+
+    }
+
 
