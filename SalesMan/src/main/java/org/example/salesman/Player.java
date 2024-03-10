bd4fc7903999290fa3387f66c766a391d190da86
Blackbeard3978 <116648728+Blackbeard3978@users.noreply.github.com>
2024-03-10 17:47:58 -0400

    Merge remote-tracking branch 'origin/master'

--- SalesMan/src/main/java/org/example/salesman/Player.java
+++ SalesMan/src/main/java/org/example/salesman/Player.java
@@ -1,11 +1,19 @@
 package org.example.salesman;
-
+//This class will hold all the important information
+//about the player's status during the game.
 public class Player {
     private int playerNumber;
+    private int playerStrength;
     private Wallet playerWallet=new Wallet();
 
     public int getPlayerNumber(){return playerNumber;}
     public void setPlayerNumber(int playerNumber){this.playerNumber=playerNumber;}
+    public int getPlayerStrength(){return playerStrength;}
+
+    public void setPlayerStrength(int playerStrength) {
+        this.playerStrength = playerStrength;
+    }
+
     public Wallet getPlayerWallet(){return playerWallet;}
     public void setPlayerWallet(Wallet playerWallet){this.playerWallet=playerWallet;}
 
