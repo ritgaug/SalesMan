5fcd68d08bd3d5cddf3f72034778c3ca690b6643
Blackbeard3978 <116648728+Blackbeard3978@users.noreply.github.com>
2024-03-10 17:44:12 -0400

    Merge remote-tracking branch 'origin/master'

---
+++ SalesMan/src/main/java/org/example/salesman/Map.java
@@ -0,0 +1,4 @@
+package org.example.salesman;
+
+public class Map {
+}
--- SalesMan/src/main/java/org/example/salesman/Player.java
+++ SalesMan/src/main/java/org/example/salesman/Player.java
@@ -1,4 +1,12 @@
 package org.example.salesman;
 
 public class Player {
+    private int playerNumber;
+    private Wallet playerWallet=new Wallet();
+
+    public int getPlayerNumber(){return playerNumber;}
+    public void setPlayerNumber(int playerNumber){this.playerNumber=playerNumber;}
+    public Wallet getPlayerWallet(){return playerWallet;}
+    public void setPlayerWallet(Wallet playerWallet){this.playerWallet=playerWallet;}
+
 }
