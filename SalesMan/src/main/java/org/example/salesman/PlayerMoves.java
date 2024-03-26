cc2e9b85b92172c23912bdb46433528bb9b61885
a.williams <awilli40@uoguelph.ca>
2024-03-26 08:56:01 -0400

    If player tries to move out of bounds, it won't use that as part of their roll

---
+++ SalesMan/src/main/java/org/example/salesman/PlayerMoves.java
@@ -0,0 +1,27 @@
+package org.example.salesman;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+public class PlayerMoves {
+    public static List<Point> pathTraveledPlayer1 = new ArrayList<>();
+    public static List<Point> pathTraveledPlayer2 = new ArrayList<>();
+
+    public PlayerMoves() {
+        player1Path();
+        player2Path();
+    }
+    public void player1Path() {
+        pathTraveledPlayer1 = new ArrayList<>();
+    }
+    public void player2Path() {
+        pathTraveledPlayer2 = new ArrayList<>();
+    }
+    public static void addTravels1(int x, int y) {
+        pathTraveledPlayer1.add(new Point(x, y));
+
+    }
+    public static void addTravels2(int x, int y) {
+        pathTraveledPlayer2.add(new Point(x, y));
+    }
+}
